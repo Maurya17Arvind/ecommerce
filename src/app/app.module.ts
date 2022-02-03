b9f18f4becb84d6342b8ae5f50640a368520e943
professor-tagline <parthpatel.tagline@gmail.com>
2022-02-03 16:39:37 +0530

    install firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+
 
 @NgModule({
   declarations: [
@@ -9,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyA5qu2zwqnox56d2sXfVfM-B2oP6KIwBAE",
+    authDomain: "ecommerce-40e3d.firebaseapp.com",
+    projectId: "ecommerce-40e3d",
+    storageBucket: "ecommerce-40e3d.appspot.com",
+    messagingSenderId: "394740174224",
+    appId: "1:394740174224:web:2614a4882f3ef254a0b715"
+  }
 };
 
 /*

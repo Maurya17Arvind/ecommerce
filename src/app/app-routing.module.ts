b4b1f266705b32a98566c913080daa571bf01a60
arvind-tagline <arvind.tagline@gmail.com>
2022-02-07 09:37:11 +0530

    Create core module with header and footer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignupComponent } from './user-signup/user-signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UserSignupComponent
+  },
+  {
+    path: 'login',
+    component: UserLoginComponent
   }
 ];
 
--- src/app/user-signup/user-signup.component.ts
+++ src/app/user-signup/user-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthAuthenticationService } from '../auth-authentication.service';
 
 @Component({
@@ -16,7 +17,7 @@ export class UserSignupComponent implements OnInit {
   public signUpPassword!: string;
 
 
-  constructor(private authenticationService: AuthAuthenticationService, private fb: FormBuilder) {
+  constructor(private authenticationService: AuthAuthenticationService, private fb: FormBuilder, private router: Router) {
     this.myForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -35,6 +36,7 @@ export class UserSignupComponent implements OnInit {
     this.signUpEmail = this.myForm.value.email;
     this.signUpPassword = this.myForm.value.password;
     this.authenticationService.signUp(this.signUpEmail, this.signUpPassword);
+    this.router.navigate(['login']);
   }
 
   get fControl() {

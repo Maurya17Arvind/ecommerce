86e6926b4a81ae6511711141705491096a1f154b
professor-tagline <parthpatel.tagline@gmail.com>
2022-02-07 16:42:33 +0530

    Create signUp and login form with validation and add firebase

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthAuthenticationService } from 'src/app/auth-authentication.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public myForm!: FormGroup;
+  public email!: string;
+  public password!: string;
+  public signUpEmail!: string;
+  public signUpPassword!: string;
+
+
+  constructor(private authenticationService: AuthAuthenticationService, private fb: FormBuilder, private router: Router) {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+
+
+  public signUp():void {
+    this.signUpEmail = this.myForm.value.email;
+    this.signUpPassword = this.myForm.value.password;
+    this.authenticationService.signUp(this.signUpEmail, this.signUpPassword);
+    this.router.navigate(['login']);
+  }
+
+  get fControl() {
+    return this.myForm.controls;
+  }
 }

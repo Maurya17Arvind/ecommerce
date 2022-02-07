2c9c7f0453e6dcb39d92c956cd5859fe6e15c30e
arvind-tagline <arvind.tagline@gmail.com>
2022-02-07 12:08:55 +0530

    Create all components for admin module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component:DashboardComponent
+  },
+  {
+    path: '',
+    loadChildren: () => import('./orders/orders.module').then((m)=>m.OrdersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

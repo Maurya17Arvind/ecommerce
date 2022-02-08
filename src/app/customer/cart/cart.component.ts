75e361331f5e4edbdd4a37de62c0a22e4c1d7fe2
professor-tagline <parthpatel.tagline@gmail.com>
2022-02-08 18:39:44 +0530

    Show cart Data

--- src/app/customer/cart/cart.component.ts
+++ src/app/customer/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ProductService } from '../customer-service/product.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public carts: any;
+  public filterCart: any;
+
+  constructor(private productService: ProductService, private db: AngularFireDatabase) {
+    const cartData = this.db.database.ref('/carts');
+    cartData.on('value', (data: any) => {
+      this.carts = Object.keys(data.val()).map(key => {
+        return {
+          ...data.val()[key],
+          cartId: key
+        }
+      });
+     this.filterCart = this.carts.filter((e: any) => e.customerID == localStorage.getItem('customerId'));
+      console.log('data', this.filterCart);
+    });
+  }
 
   ngOnInit(): void {
   }

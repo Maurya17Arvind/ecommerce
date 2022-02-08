88db4d956ad01608309e4631b94f12797670389a
arvind-tagline <arvind.tagline@gmail.com>
2022-02-08 09:56:47 +0530

    view item from firebase

--- src/app/admin/view-items/view-items.component.ts
+++ src/app/admin/view-items/view-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-view-items',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewItemsComponent implements OnInit {
 
-  constructor() { }
+  public products: any;
+
+  constructor(private db: AngularFireDatabase) {
+    // this.products = db.list('/products').valueChanges();
+    // this.products.subscribe((products: any) => {
+    //   this.allProducts = products;
+    //   console.log(this.allProducts);
+    // })
+    let allProducts = this.db.database.ref('/products/');
+    allProducts.on('value', (data: any) => {
+      // console.log('data.val()', data.val());
+      this.products = Object.keys(data.val()).map(key => {
+        return {
+          ...data.val()[key],
+          push_key: key
+        }
+      });
+      console.log('this.products', this.products);
+    });
+  }
 
   ngOnInit(): void {
   }

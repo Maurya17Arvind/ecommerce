9f20cf1733af2c67ad2b1dcdaaebdf9b6c21e1b0
arvind-tagline <arvind.tagline@gmail.com>
2022-02-08 11:52:43 +0530

    Update Products with firebase

--- src/app/admin/update-item/update-item.component.ts
+++ src/app/admin/update-item/update-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UpdateData } from 'src/app/InterFace/all-interface';
 
 @Component({
   selector: 'app-update-item',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateItemComponent implements OnInit {
 
-  constructor() { }
+  public myForm!: FormGroup;
+  public key!: string;
+  public updateProduct: any;
+  public updateData!: UpdateData;
+
+  constructor(private activatedRoute: ActivatedRoute, private fb: FormBuilder, private db: AngularFireDatabase, private router: Router) {
+    this.key = this.activatedRoute.snapshot.params['id']
+    this.updateProduct = this.db.database.ref('/products/' + this.key);
+    this.updateProduct.on('value', (data: any) => {
+      this.updateData = data.val();
+      console.log('updateData', this.updateData);
+    })
+    // console.log('this.key', this.key);
+    this.myForm = this.fb.group({
+      itemName: [this.updateData.itemName],
+      price: [this.updateData.price],
+      returnTime: [this.updateData.returnTime]
+    })
+
+  }
 
   ngOnInit(): void {
   }
 
+
+  public updateItem(): void {
+    this.updateProduct.update(this.myForm.value);
+    this.router.navigate(['/view-items']);
+  }
 }

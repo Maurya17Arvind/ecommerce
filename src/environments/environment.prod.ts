a0efe43beb28a91eb6cf1cba7188beef517988c5
arvind-tagline <arvind.tagline@gmail.com>
2022-02-15 15:34:52 +0530

    firebase-tools setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5qu2zwqnox56d2sXfVfM-B2oP6KIwBAE",
+    authDomain: "ecommerce-40e3d.firebaseapp.com",
+    projectId: "ecommerce-40e3d",
+    storageBucket: "ecommerce-40e3d.appspot.com",
+    messagingSenderId: "394740174224",
+    appId: "1:394740174224:web:2614a4882f3ef254a0b715"
+  }
 };

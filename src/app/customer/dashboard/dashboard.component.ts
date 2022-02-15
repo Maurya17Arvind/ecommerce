0d68f256381b8ce23a3220cf520e8d6ba1a1372b
arvind-tagline <arvind.tagline@gmail.com>
2022-02-15 11:38:50 +0530

    Add product dasboard to cart

--- src/app/customer/dashboard/dashboard.component.ts
+++ src/app/customer/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../customer-service/product.service';
 
 @Component({
@@ -10,15 +9,37 @@ import { ProductService } from '../customer-service/product.service';
 })
 export class DashboardComponent implements OnInit {
 
-  products: any;
+  public products: any = [];
+  public key!: string;
+  public product: any;
+  public productsData: any;
+  public finalProductData: any;
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private db: AngularFireDatabase
+  ) {
     this.products = this.productService.getAllProducts();
-    // console.log('this.products :>> ', this.products);
+    console.log('this.products', this.products);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public addToCart(key: any): void {
+    const cart = this.products.find((e: any) => e.push_key = key)
+    this.product = this.db.database.ref('/products/' + cart.push_key);
+    this.product.on('value', (data: any) => {
+      this.productsData = data.val();
+      this.finalProductData = {
+        ...this.productsData,
+        qty: 1,
+      };
+    });
+    const productArray = {
+      ...this.finalProductData,
+      product_id: cart,
+      finalPrice: this.finalProductData.price * this.finalProductData.qty
+    };
+    this.productService.addToCart(productArray);
   }
-
-
 }

6ca4ca988daadd95641c7d93bc6991509e8ebba7
arvind-tagline <arvind.tagline@gmail.com>
2022-02-16 15:41:32 +0530

    Create customer Profile

--- src/app/customer/profile/profile.component.ts
+++ src/app/customer/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  public orders: any;
+  public filterOrder: any;
+  public addresses: any;
+  address: any;
+  area: any;
+  pinCode: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    const cartData = this.db.database.ref('/orders');
+    cartData.on('value', (data: any) => {
+      this.orders = Object.keys(data?.val() || '').map(key => {
+        return {
+          ...data.val()[key],
+          orderId: key
+        }
+      });
+      this.filterOrder = this.orders.filter((e: any) => e.userId == localStorage.getItem('customerId'))
+      this.filterOrder.filter((e: any) => {
+        this.addresses = e;
+        this.address = e.address;
+        this.area = e.area;
+        this.pinCode = e.pinCode;
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
